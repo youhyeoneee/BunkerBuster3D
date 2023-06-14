4ded27decd2bd995fd3b35dba6b6ac5dc51a1014
ktgokr <vipkeon@naver.com>
2023-06-14 16:37:32 +0900

    Dance_FlyAway

--- Assets/Scripts/Scripts_Hecter/ExplosionTst.cs
+++ Assets/Scripts/Scripts_Hecter/ExplosionTst.cs
@@ -6,7 +6,24 @@ public class ExplosionTst : MonoBehaviour
 {
     [SerializeField] float force;
     [SerializeField] float radius;
-    
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Y))
+        {
+            Vector3 explotionPos = transform.position;
+            Collider[] colliders = Physics.OverlapSphere(explotionPos, radius);
+            foreach (Collider collider in colliders)
+            {
+                Rigidbody rb = collider.GetComponent<Rigidbody>();
+                if (rb != null)
+                {
+                    rb.AddExplosionForce(force, explotionPos, radius, 0.05f, ForceMode.Impulse);
+                }
+            }
+        }
+    }
+
     void OnCollisionEnter(Collision other)
     {
         Vector3 explotionPos = transform.position;
